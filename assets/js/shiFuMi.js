453e3942eefebc6701f8af9a36fb0f777420e89b
Your Name <jesaispas@github.com>
2023-06-20 16:51:57 +0200

    Fonctionnel

--- assets/js/shiFuMi.js
+++ assets/js/shiFuMi.js
@@ -1,13 +1,23 @@
-// JEU : pierre, feuille, ciseaux avec les elements feu glace et terre
+// ------------------------JEU : pierre, feuille, ciseaux avec les elements feu, terre et glace.----------------
+// PIERRE = FIRE
+// PAPIER = SOIL
+// CISEAUX = ICE
 let resetBtn = document.getElementById("reset");
 let scoreJoueur = document.getElementById("score-joueur");
 let scoreOrdinateur = document.getElementById("score-ordinateur");
+let scoreEgalité = document.getElementById("score-égalité");
 let btnJoueur = [...document.getElementsByClassName("btn-joueur")];
 let opierreBtn = document.getElementById("opierre");
 let opapierBtn = document.getElementById("opapier");
 let ociseauxBtn = document.getElementById("ociseaux");
 let message = document.getElementById("message");
 let nextBtn = document.getElementById("next");
+let pierre = document.querySelector("#pierre")
+let papier = document.querySelector("#papier")
+let ciseaux = document.querySelector("#ciseaux")
+
+
+// --------------JOUER MANCHE---------------------
 
 const jouerManche = (e) => {
     let choix = e.target.closest(".btn-joueur");
@@ -23,23 +33,20 @@ const jouerManche = (e) => {
 
     let choixOrdi = faireChoixOrdinateur();
 
-    verifierGagnant(choixJoueur, choixOrdi)
+    verifierGagnant(choixJoueur, choixOrdi);
     nextBtn.style.visibility = "visible";
 };
 
-// PIERRE = FIRE
-// PAPIER = SOIL
-// CISEAUX = ICE
-
+//---------- RANDOM ORDINATEUR--------------------
 
 const PIERRE = "pierre";
 const PAPIER = "papier";
 const CISEAUX = "ciseaux";
 
 const faireChoixOrdinateur = () => {
-    // 0 = pierre
-    // 1 = papier
-    // 2 = ciseaux
+    // 0 = pierre / feu
+    // 1 = papier / terre
+    // 2 = ciseaux / glace
     let nbAleatoire = Math.floor(Math.random() * 3);
     switch(nbAleatoire){
         case 0 :
@@ -48,52 +55,76 @@ const faireChoixOrdinateur = () => {
         case 1 :
             opapierBtn.classList.add("active");
             return PAPIER;
-        case 2 :
+        default :
             ociseauxBtn.classList.add("active");
             return CISEAUX;
-
     }
 };
 
-const victoireOrdinateur = () =>{
+//---------- COMPTEUR VICTOIRES/DEFAITES--------------------
+
+
+const victoireOrdinateur = () => {
     message.textContent = "Vous avez perdu...";
     scoreOrdinateur.textContent++;
-}
-const victoireJoueur = () =>{
+};
+const victoireJoueur = () => {
     message.textContent = "Vous avez gagné !";
     scoreJoueur.textContent++;
-}
+};
+const egalité = () => {
+    message.textContent = "Egalité !";
+    scoreEgalité.textContent++;
+};
+
+//---------- COMPARATEUR VALEUR POUR SAVOIR QUI GAGNE-------------------
+
 const verifierGagnant = (choixJoueur, choixOrdi) => {
     if (choixJoueur == choixOrdi){
-        MessageChannel.textContent = 'Egalité !';
+        return egalité ();
     }
-    if (choixJoueur = PIERRE){
-        if (choixOrdi == PAPIER)
-        return victoireOrdinateur();
-    } else if (choixOrdi  == CISEAUX){
-        return victoireJoueur();
-    }
-    if (choixJoueur = PAPIER){
-        if (choixOrdi == CISEAUX)
-        return victoireOrdinateur();
-    } else if (choixOrdi  == PIERRE){
-        return victoireJoueur();
-    }
-    if (choixJoueur = CISEAUX){
-        if (choixOrdi == PIERRE)
+    if (choixJoueur == PIERRE) {
+        if (choixOrdi == PAPIER) {
+            // pierre.style.backgroundColor = "black";
+          return victoireOrdinateur();
+        } else if (choixOrdi == CISEAUX) {
+            // ociseauxBtn.style.backgroundColor = "black";
+          return victoireJoueur();
+        }
+      }
+      if (choixJoueur == PAPIER) {
+        if (choixOrdi == CISEAUX) {
+            // papier.style.backgroundColor = "black";
+          return victoireOrdinateur();
+        } else if (choixOrdi == PIERRE) {
+            // opierreBtn.style.backgroundColor = "black";
+          return victoireJoueur();
+        }
+      }
+    if (choixJoueur == CISEAUX){
+        if (choixOrdi == PIERRE) {
+            // ciseaux.style.backgroundColor = "black";
         return victoireOrdinateur();
     } else if (choixOrdi  == PAPIER){
+        // opapierBtn.style.backgroundColor = "black";
         return victoireJoueur();
     }
+    }
 };
+
+
+//---------- NOUVELLE MANCHE--------------------
+
+
 const preparerNouvelleManche = () => {
     btnJoueur.forEach((btn) =>{
         btn.classList.remove("desactivated");
         btn.classList.remove("active");
-
         btn.addEventListener("click", jouerManche);
     });
-
+    // pierre.style.display = 'block';
+    // papier.style.display = 'block';
+    // ciseaux.style.display = 'block';
     nextBtn.style.visibility = "hidden";
     opierreBtn.classList.remove("active");
     opapierBtn.classList.remove("active");
@@ -103,69 +134,10 @@ const preparerNouvelleManche = () => {
 
 
 nextBtn.addEventListener("click", preparerNouvelleManche);
-
 btnJoueur.forEach(btn => btn.addEventListener("click", jouerManche));
-
 resetBtn.addEventListener("click", () => {
 scoreJoueur.textContent = 0;
 scoreOrdinateur.textContent = 0;
+scoreEgalité.textContent = 0;
 preparerNouvelleManche();
 });
-
-function choicePierre(){
-    console.log('Pierre est selectionné');
-    PAPIER.style.display = 'none';
-    CISEAUX.style.display = 'none';
-}
-function choicePapier(){
-    console.log('Papier est selectionné');
-    PIERRE.style.display = 'none';
-    CISEAUX.style.display = 'none';
-}
-function choiceCiseaux(){
-    console.log('Ciseaux est selectionné');
-    PAPIER.style.display = 'none';
-    PIERRE.style.display = 'none';
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let pierre = document.querySelector('.pierre');
-// let papier = document.querySelector('.papier');
-// let ciseaux = document.querySelector('.ciseaux');
-// let randChoice = "";
-// let win = document.querySelector('.win');
-// let lose = document.querySelector('.lose');
-// let egalité = document.querySelector('.égalité');
-// let result = "";
-// let nbVictoire = "";
-// let nbDéfaite = "";
-// let nbEgalité = "";
-// let nbParties = "";
-// // Choisir pierre papier ou ciseaux
-// function choicePierre(){
-//         console.log('Pierre est selectionné');
-//         papier.style.display = 'none';
-//         ciseaux.style.display = 'none';
-// }
-// function choicePapier(){
-//         console.log('Papier est selectionné');
-//         pierre.style.display = 'none';
-//         ciseaux.style.display = 'none';
-// }
-// function choiceCiseaux(){
-//         console.log('Ciseaux est selectionné');
-//         papier.style.display = 'none';
-//         pierre.style.display = 'none';
-// };
